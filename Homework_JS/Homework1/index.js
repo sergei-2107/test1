6f6cdb1c69f3a7d9c435efbbb64c88ee26f76531
serg2107 <shpak_sergei2107@mail.ru>
2025-02-04 00:19:36 +0300

    work1

--- Homework_JS/Homework1/index.js
+++ Homework_JS/Homework1/index.js
@@ -1,4 +1,95 @@
+// const str1 = 'hello ';
+// const str2 = 'world';
+//                                 // задача 1
+// const sum = str1 + str2;
+// console.log(sum);
 
+
+
+
+
+
+// const str = 'I learn computer science';
+//                                             // задача 2
+// const str1 = str.replace(/ /g, '_');    
+// console.log(str1);
+
+
+
+
+
+
+// const str = 'I learn computer science';
+//                                                 // задача 3
+// const str1 = str.length;                    
+
+// console.log(str1);
+
+
+
+
+
+// const min = 42;
+
+// // if (0 <= min <= 14, 15 <= min <= 29, 30 <= min <= 44, 45 <= min <= 59) {
+// //     alert ('третья четверть')
+// // }
+
+
+
+
+// if (0 <= min <= 14) {
+//     alert ('ne pervaya chetvert');
+// }
+// else  {
+//     alert ('pervaya chetvert');
+// };
+
+// if (15 <= min <= 29) {
+//     alert ('ne vtoraya chetvrt');
+// }
+// else  {
+//     alert ('vtoraya chetvert');
+// };
+//                                         // задача 4
+// if (30 <= min <= 44) {
+//     alert ('tretaya chetvert');
+// }
+// else  {
+//     alert ('ne tretaya chetvert');
+// };
+
+// if (45 <= min <= 59) {
+//     alert ('ne chetvertya chetvert')
+// }
+// else  {
+//     alert ('chetvertya chetvert')
+// };
+
+
+
+// const days1 = 'ru';
+
+// if (days1 === 'ru') {
+//    console.log('Понедельник');
+//    console.log('Вторник');
+//    console.log('Среда');
+//    console.log('Четверг');
+//    console.log('Пятница');
+//    console.log('Суббота');
+//    console.log('Воскресенье');
+//                                             // задача 5
+// };
+// if (days1 =='ru') {
+//     console.log('Monday');
+//     console.log('Tuesday');
+//     console.log('Wednesday');
+//     console.log('Thursday');
+//     console.log('Friday');
+//     console.log('Saturday');
+//     console.log('Sunday');
+    
+// }
 
 
 
