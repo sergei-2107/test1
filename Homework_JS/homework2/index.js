d6c21d0fadd1b4ab25ca3d562d92957d3bde46f2
serg2107 <shpak_sergei2107@mail.ru>
2025-02-17 22:09:57 +0300

    homework2

---
+++ Homework_JS/homework2/index.js
@@ -0,0 +1,94 @@
+// const arr = [1,1,2,2,3,3,4,5,5,5,6,6,7,7,8,8,];
+
+// // const uniqueElements = arr.filter((item, index) => arr.indexOf(item) === index);
+
+// // console.log(uniqueElements);
+
+// const uniArr = [...new Set(arr)];
+
+// console.log(uniArr);
+
+
+
+
+
+
+// const n = 7;
+
+// function factorial(n) {
+//     return (n != 1) ? n * factorial(n - 1) : 1;
+// }
+
+// console.log(factorial(n));
+
+
+
+
+
+
+
+
+
+// const year = 2023;
+
+// if (year % 4 === 0) {
+//     console.log(`visokosny`);
+    
+// }else {
+//     console.log(`ne visokosny`);
+    
+// }
+
+
+
+
+
+
+
+// const arr = [1,2,2,1,7,4,2,5,4,7,5,1];
+
+// const arr1 = arr.reduce((a, i) => {
+//     if (!a[i]) {
+//         a[i] = 1;
+//     } else {
+//         a[i]++;
+//     }
+//     return a;
+// },{})
+
+// console.log(arr1);
+
+
+
+
+
+
+
+
+
+// let arr = [21,2,43,32,56,46,88,29,65,91];
+
+// arr = Math.floor(Math.random() * 101);
+
+// console.log(arr);
+
+
+
+
+
+
+
+
+// const arr = [
+//     {name: 'John', age: 16},
+//     {name: 'Jane', age: 30},
+//     {name: 'Bob', age: 28},
+//     {name: 'Alice', age: 32},
+//     {name: 'Anna', age: 17}
+// ];
+
+// const filteredArr = arr.filter(item => item.age >= 18);
+
+// console.log(filteredArr);
+
+
